8740d6c18a04ad0db321a67ccab4ed0adde78415
Jacek Krużycki <kruzyk@gmail.com>
2018-11-19 23:53:11 +0100

    adds Facebook Like button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-end">
-        <a
+        {/* <a
           className="navbar-item"
           href="https://www.facebook.com/MaryJaneRazorKaraoke/"
           target="_blank"
@@ -37,7 +37,17 @@ const Navbar = () => (
           <span className="icon">
             <img src={facebook} alt="Facebook" />
           </span>
-        </a>
+        </a> */}
+        <div id="fb-root"></div>
+        <script>{`(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v3.2&appId=226864780659472&autoLogAppEvents=1';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));`}</script>
+        <div class="fb-like" data-href="https://www.facebook.com/MaryJaneRazorKaraoke/" data-layout="button" data-action="like" data-size="large" data-show-faces="true" data-share="false"></div>
+
       </div>
     </div>
   </nav>
