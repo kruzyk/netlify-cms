bbcec354fa4aa85b0d5ec3f20bcd4b3c72f46da2
Jacek Krużycki <kruzyk@gmail.com>
2018-11-19 17:32:48 +0100

    change 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import Layout from '../components/Layout'
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>Smuteczek</h1>
+      <p>Nie wiem co chciałeś znaleźć ale tutaj tego nie ma...</p>
     </div>
   </Layout>
 )
