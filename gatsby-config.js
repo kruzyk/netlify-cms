038f344569322d8d9db3bb9e2cb6081b29542b31
Jacek Krużycki <kruzyk@gmail.com>
2018-11-19 22:16:31 +0100

    adds SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby + Netlify CMS Starter',
+    titleTemplate: "%s · The Real Hero",
+    description:
+      "Hogwarts Potions master, Head of Slytherin house and former Death Eater.",
+    url: "https://www.karaoke-3city.pl", // No trailing slash allowed!
+    image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@occlumency",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
