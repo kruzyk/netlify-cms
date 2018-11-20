967d70c12a5ae0ad8360a2af591983bbdb53f770
Jacek Krużycki <kruzyk@gmail.com>
2018-11-20 19:57:43 +0100

    move Google Analytics code to snippet at Netlify

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,18 +6,8 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Karaoke Party w Trójmieście | Prowadzenie imprez w Gdańsku, Sopocie i Gdyni">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129155228-1"></script>
-      <script>{`
-        window.dataLayer = window.dataLayer || [];
-       function gtag(){dataLayer.push(arguments);}
-       gtag('js', new Date());
-
-       gtag('config', 'UA-129155228-1');`}
-    </script>
-    </Helmet>
+    <Helmet title="Karaoke Party w Trójmieście | Prowadzenie imprez w Gdańsku, Sopocie i Gdyni" />
     <Navbar />
-
     <div>{children}</div>
   </div>
 )
