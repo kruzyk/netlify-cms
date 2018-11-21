ae77896589d43b41e795320ffda9520a83dc8de5
Jacek Krużycki <kruzyk@gmail.com>
2018-11-21 21:06:43 +0100

    adds gzip

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,3 +85,27 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+const fs = require('fs');
+const zlib = require('zlib');
+const iltorb = require('iltorb');
+const glob = require('glob');
+
+exports.onPostBuild = () =>
+  new Promise((resolve, reject) => {
+    try {
+      const publicPath = path.join(__dirname, 'public');
+      const gzippable = glob.sync(`${publicPath}/**/?(*.html|*.js|*.css|*.svg)`);
+      gzippable.forEach((file) => {
+        const content = fs.readFileSync(file);
+        const zipped = zlib.gzipSync(content);
+        fs.writeFileSync(`${file}.gz`, zipped);
+        
+        const brotlied = iltorb.compressSync(content);
+        fs.writeFileSync(`${file}.br`, brotlied);
+      });
+    } catch (e) {
+      reject(new Error('onPostBuild: Could not compress the files'));
+    }
+
+    resolve();
+  });
