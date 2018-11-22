7d7f4e0b868bcf0d453a71bcb0b1b5fa5914660f
Jacek Krużycki <35242828+kruzyk@users.noreply.github.com>
2018-11-22 02:59:19 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,15 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Karaoke Party w Trójmieście | Prowadzenie imprez w Gdańsku, Sopocie i Gdyni" />
+    <Helmet
+      title="Karaoke Party w Trójmieście | Prowadzenie imprez w Gdańsku, Sopocie i Gdyni"
+      meta={[
+        { name: 'description', content: '>> Najlepsze karaoke w Trójmieście! << Zobacz gdzie jest impreza. Wiele lat doświadczenia w prowadzeniu imprez tanecznych i wesel.' },
+        { name: 'keywords', content: 'karaoke, impreza, party, trójmiasto, gdańsk, sopot, gdynia' },
+      ]}
+    >
+      <html lang="pl" />
+    </Helmet>
     <Navbar />
     <div>{children}</div>
   </div>
