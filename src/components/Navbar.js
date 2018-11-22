35f02e52ea0553ddec18e8d5aa5d389da6184114
Jacek Krużycki <35242828+kruzyk@users.noreply.github.com>
2018-11-22 01:16:25 +0100

    messes with FB like button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = () => (
           </span>
         </a>
         <div
-          className="fb-like icon"
+          className="navbar-item icon fb-like"
           data-href="https://www.facebook.com/MaryJaneRazorKaraoke/"
           data-layout="button"
           data-action="like"
